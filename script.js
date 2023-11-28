39129c0ec4773a963825e029422489b6d15c6d85
Athul18Jose <athul18jose@gmail.com>
2023-11-29 00:08:30 +0530

    weather

---
+++ script.js
@@ -0,0 +1,58 @@
+const inputBox = document.querySelector('.input-box');
+const searchBtn = document.getElementById('searchBtn');
+const weather_img = document.querySelector('.weather-img');
+const temperature = document.querySelector('.temperature');
+const description = document.querySelector('.description');
+const humidity = document.getElementById('humidity');
+const wind_speed = document.getElementById('wind-speed');
+
+const location_not_found = document.querySelector('.location-not-found');
+
+const weather_body = document.querySelector('.weather-body');
+
+async function checkWeather() {
+    const city = inputBox.value; // Corrected variable name
+
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=a08861f537ff33a1623d35efddba8b86`;
+
+    const weather_data = await fetch(url).then(response => response.json());
+
+    if (weather_data.cod === '404') {
+        location_not_found.style.display = 'flex';
+        weather_body.style.display = 'none';
+        console.log('error');
+        return;
+    }
+
+    location_not_found.style.display = 'none';
+    weather_body.style.display = 'flex';
+    temperature.innerHTML = `${Math.round(weather_data.main.temp - 273.15)}°C`;
+    description.innerHTML = `${weather_data.weather[0].description}`;
+
+    humidity.innerHTML = `${weather_data.main.humidity}%`;
+    wind_speed.innerHTML = `${weather_data.wind.speed}Km/H`;
+
+    switch (weather_data.weather[0].main) {
+        case 'Clouds':
+            weather_img.src = 'https://i.postimg.cc/YSBjT8Zc/cloud.png'; // Corrected image path
+            break;
+        case 'Clear':
+            weather_img.src = 'https://i.postimg.cc/pTTvSFWb/clear.png'; // Corrected image path
+            break;
+        case 'Rain':
+            weather_img.src = 'https://i.postimg.cc/Jzx4dmNX/rain.png'; // Corrected image path
+            break;
+        case 'Mist':
+            weather_img.src = 'https://i.postimg.cc/jjhqmTyN/mist.png'; // Corrected image path
+            break;
+        case 'Snow':
+            weather_img.src = 'https://i.postimg.cc/pXxvN4XJ/snow.png'; // Corrected image path
+            break;
+    }
+
+    console.log(weather_data);
+}
+
+searchBtn.addEventListener('click', () => {
+    checkWeather(inputBox.value);
+});
